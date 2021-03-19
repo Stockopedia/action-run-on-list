d831cc5bf3dd124231fc0411023a34202bb155b8
Tj Broodryk <tjaart@stockopedia.com>
2021-03-19 13:36:13 +0000

    test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { exec } from 'child_process'
+import {exec} from 'child_process'
 
 async function run(): Promise<void> {
   try {
@@ -8,9 +8,8 @@ async function run(): Promise<void> {
     const inputCommand: string = core.getInput('command')
 
     for (const item of list) {
-      core.info(await promiseExec(`${inputCommand}`, { item }));
+      core.info(await promiseExec(`${inputCommand}`, {item}))
     }
-
   } catch (error) {
     core.setFailed(error.message)
   }
@@ -18,13 +17,16 @@ async function run(): Promise<void> {
 
 run()
 
-function promiseExec(command: string, env?: {[key: string]: string}): Promise<string> {
+function promiseExec(
+  command: string,
+  env?: {[key: string]: string}
+): Promise<string> {
   return new Promise<string>((res, reject) => {
-    exec(command, { env }, function(error, stdout) {
-      if(error) {
+    exec(command, {env}, function (error, stdout) {
+      if (error) {
         return reject(error)
-      }  
+      }
       return res(stdout)
-    });
+    })
   })
-} 
+}
